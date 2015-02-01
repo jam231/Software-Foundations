839534f20f99dab943ae23a570fee4ff0c76573f
jam231 <mateusz.jany@gmail.com>
2015-02-01 17:29:30 +0100

    Add solution to Basics.v - negation_fn_applied_twice.

--- Basics.v
+++ Basics.v
@@ -889,13 +889,25 @@ Theorem identity_fn_applied_twice :
   (forall (x : bool), f x = x) ->
   forall (b : bool), f (f b) = b.
 Proof.
-  (* FILL IN HERE *) Admitted.
+  intros f H_f_id b.
+  repeat (rewrite H_f_id).
+  reflexivity.
+Qed.
+
 
 (** Now state and prove a theorem [negation_fn_applied_twice] similar
     to the previous one but where the second hypothesis says that the
     function [f] has the property that [f x = negb x].*)
-
-(* FILL IN HERE *)
+Theorem negation_fn_applied_twice :
+  forall (f : bool -> bool),
+  (forall (x : bool), f x = negb x) ->
+  forall (b : bool), f (f b) = b.
+Proof.
+  intros f H_f_negb b.
+  repeat (rewrite H_f_negb).
+  rewrite negb_involutive.
+  reflexivity.
+Qed.
 (** [] *)
 
 (** **** Exercise: 2 stars (andb_eq_orb)  *)
